2ef055f6fa3283f70923ffe59ad08426d8ad4470
heizshubham <shubhamp01947@gmail.com>
2025-12-20 17:59:10 +0530

    first ques

---
+++ 2667 helloworld.js
@@ -0,0 +1,22 @@
+// function within function
+
+// function fun(){
+//     function f(a,b){
+//         const sum = a+b
+//         return sum
+
+//     }
+//     return f
+// }
+
+// var result = fun()
+// console.log(result(2,3)) 
+
+
+
+function createHelloWorld(){
+    function f(){
+        return "Hello World"
+    }
+    return f
+}
