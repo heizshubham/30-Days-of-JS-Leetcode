024147aa884e80ff6f29aa166c655a8fd39205d9
heizshubham <shubhamp01947@gmail.com>
2025-12-31 02:06:23 +0530

    ques 8 functionComposition

--- 2629 functionComposition.js
+++ 2629 functionComposition.js
@@ -4,14 +4,14 @@
 
 // Here's the implementation:
 
-var compose = function(func, arr) {
-    const newarr = []   // Create a new array to store results
-    for(let i=0; i<arr.length; i++){
-        // Apply the function to the current element
-        newarr[i] = func(arr[i])
-    }
-    return newarr  // Return the newly created array
-};
+// var compose = function(func, arr) {
+//     const newarr = []   // Create a new array to store results
+//     for(let i=0; i<arr.length; i++){
+//         // Apply the function to the current element
+//         newarr[i] = func(arr[i])
+//     }
+//     return newarr  // Return the newly created array
+// };
 
 
 
@@ -20,5 +20,71 @@ var compose = function(func, arr) {
 // ------------------------------------------
 
 
+// function add(args){
+//     return args[0] + args[1]
+// }
+
+// function mulTwo(val){
+//     return val * 2
+// }
+// function square(val){
+//     return val * val
+// }
+
+// const result = add(3, 4)  // output : 7
+// console.log(square(result))  // output : 49
+
+// console.log(addsquare(3,4))  // output : 49
 
 
+
+// function compose(f1, f2){
+//     return function(a,b){
+//         return f2(f1(a,b))
+//     }
+// }
+
+// or using arrow function
+
+// const composeTwo = (f1, f2, f3) => (a,b) => f3(f2(f1(a,b)))
+
+// const result = composeTwo(add, mulTwo, square)  // output : function
+// console.log(result(3,4))  // output : 14
+
+
+
+// composition of unlimited functions
+
+// function composeAll(...funs){
+//     return function(...values){
+//         return funs.reduce((val, fn) => fn(val), values)
+//     }
+// }
+
+// or using arrow function
+
+// const composeAll = (...funs) => (...values) => funs.reduce((val, fn) => fn(val), values)
+
+
+// const result = composeAll(add, mulTwo, square)  // output : function
+// console.log(result(4,6))  // output : 100
+
+
+
+
+
+
+
+
+
+// ----------------------------------------------------------
+
+var compose = function(functions) {
+    
+    return function(x) {
+        for(let i = functions.length -1; i>=0; i--){
+            x = functions[i](x)
+        }
+        return x
+    }
+};
